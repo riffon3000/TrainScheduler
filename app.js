e9735028fbfa499dd4bbee735ef075d9fa65a395
riffon3000 <riffon@gmail.com>
2018-09-30 18:59:48 -0500

    update id's and link to grab input variables

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ firebase.initializeApp(config);
 
 // handle for database
 var database = firebase.database();
+
+// Click event listener for add-train button w/prevent form submission upon page load
+$("#add-train").on("click", function(event) {
+    event.preventDefault();
+
+    // grab user inputs
+    var trainName = $("#train-input").val().trim();
+    console.log(trainName);
+    var trainDest = $("#dest-input").val().trim();
+    console.log(trainDest);
+    var trainFirst = moment($("#first-input").val(), "h:mm a").format("X"); 
+    console.log(trainFirst);
+    var trainFreq = $("#freq-input").val();
+    console.log(trainFreq);
+}
